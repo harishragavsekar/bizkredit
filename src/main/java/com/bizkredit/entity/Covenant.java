3d6462a2805681f5f91dbcd4227062f1f1cc46f6
Dileep2507355 <GummaDileep.Kumar@cognizant.com>
2026-06-15 12:03:59 +0530

    added Active

--- src/main/java/com/bizkredit/entity/Covenant.java
+++ src/main/java/com/bizkredit/entity/Covenant.java
@@ -1,14 +1,27 @@
 package com.bizkredit.entity;
 
+import java.math.BigDecimal;
+
 import com.bizkredit.enums.CovenantStatus;
 import com.bizkredit.enums.CovenantType;
 import com.bizkredit.enums.MonitoringFrequency;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "covenant")
