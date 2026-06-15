0a331baebbe54891191758262a8f059461b26ae1
Dileep2507355 <GummaDileep.Kumar@cognizant.com>
2026-06-15 11:42:04 +0530

    Changed in WorkingCapitalUtilisation

--- src/main/java/com/bizkredit/repository/WorkingCapitalUtilisationRepository.java
+++ src/main/java/com/bizkredit/repository/WorkingCapitalUtilisationRepository.java
@@ -1,13 +1,29 @@
 package com.bizkredit.repository;
 
 import com.bizkredit.entity.WorkingCapitalUtilisation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkingCapitalUtilisationRepository extends JpaRepository<WorkingCapitalUtilisation, Long> {
 
+    @EntityGraph(attributePaths = {
+            "facility",
+            "facility.application",
+            "facility.application.business",
+            "facility.business"
+    })
+    Optional<WorkingCapitalUtilisation> findById(Long id);
+
+    @EntityGraph(attributePaths = {
+            "facility",
+            "facility.application",
+            "facility.application.business",
+            "facility.business"
+    })
     List<WorkingCapitalUtilisation> findByFacility_FacilityId(Long facilityId);
 }
